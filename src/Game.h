6eb850ddd4f5efaf90eca3c361a6727ee9470472
Chris <christopher.liebsch@st.ovgu.de>
2023-02-19 17:06:27 +0100

    private window, ev, videomode, initVariables, initWindow public Game, ~Game, getWindowIsOpen, update, render

---
+++ src/Game.h
@@ -0,0 +1,46 @@
+#pragma once 
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+
+/*
+Class for Game Engine
+*/
+
+class Game
+{
+private:
+    //window 
+    sf::RenderWindow* window;
+    sf::Event ev;
+    sf::VideoMode videomode;
+
+
+    //private Functions
+    void initVariables();
+    void initWindow();
+
+public:
+    //constructor / deconstructor
+    Game(/* args */);
+    virtual ~Game();
+    //getter
+    const bool getWindowIsOpen();
+
+    //Functions
+    void update();
+    void render();
+
+};
+
+Game::Game(/* args */)
+{
+}
+
+Game::~Game()
+{
+}
