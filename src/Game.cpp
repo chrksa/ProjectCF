da5e38dd3fd49cade45a6d8e799546643e43be2c
Chris <christopher.liebsch@st.ovgu.de>
2023-02-20 15:26:00 +0100

    update and render implemented

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "Game.h"
 
-void Game:: initVariables()
+void Game::initVariables()
 {
     this->window=nullptr;
 }
@@ -33,12 +33,45 @@ Game::~Game()
     delete this->window;
 }
 
+void Game::pollEvents()
+{
+    while (this->window->pollEvent(this->ev))
+        {
+            switch (this->ev.type)
+            {
+            case sf::Event::Closed:
+                //maybe a window with -- are you sure you wann leave the game?
+                this->window->close();
+                break;
+            case sf::Event::KeyPressed:
+                if(ev.key.code == sf::Keyboard::Escape)
+                    this->window->close();
+
+                break;
+            default:
+                break;
+            }
+        }
+}
+
+
 void Game::update()
 {
-
+    this->pollEvents();
 }
 
 void Game::render()
-{
+{   
+    /*
+    Renders game objects
+    -clear widnow 
+    -render objects
+    -display frame in window
+    */
 
+    this->window->clear(sf::Color(255,0,0,255));
+
+    //Draw game objects here
+
+    this->window->display();
 }
