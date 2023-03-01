8b96989394c8fb6b1da779fe83d3d21927ae3e69
Chris <christopher.liebsch@st.ovgu.de>
2023-03-01 21:29:59 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
 #include "Game.h"
 
 int main(int argc, char const *argv[])
