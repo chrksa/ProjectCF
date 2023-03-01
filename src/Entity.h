245a3929997c7d8786c5de1b4c6a346d673383fc
Chris <christopher.liebsch@st.ovgu.de>
2023-03-01 21:57:24 +0100

    created Entity.h and Map.h

---
+++ src/Entity.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+class Entity
+{
+private:
+    bool movable;
+    bool solid;
+    int speed;
+public:
+    Entity(/* args */);
+    ~Entity();
+};
+
+Entity::Entity(/* args */)
+{
+    
+}
+
+Entity::~Entity()
+{
+}
---
+++ src/Map.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Map
+{
+private:
+    /* data */
+    
+public:
+    Map(/* args */);
+    Map(int mapSize);
+    ~Map();
+};
+
+Map::Map(/* args */)
+{    
+}
+Map::Map(int mapSize)
+{
+}
+
+Map::~Map()
+{
+}
