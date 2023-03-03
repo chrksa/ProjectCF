96d7669105448d5bf4673b7093658d3bdec8b806
Chris <christopher.liebsch@st.ovgu.de>
2023-03-03 14:43:59 +0100

    created 3 constructors

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include "Map.h"
+#include "Game.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
+
+
+Map::Map()
+{   
+    //default values
+    this->mapHeight= 640;
+    this->mapWidth=480;
+}
+
+Map::Map(int mapSize)
+{
+    this->mapHeight=mapSize;
+    this->mapWidth=mapSize;
+}
+
+Map::Map(int mapHeight, int mapWidth)
+{
+    this->mapHeight=mapHeight;
+    this->mapWidth=mapWidth;
+}
