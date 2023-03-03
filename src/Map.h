d052980e9ce3d4467f7fd466b5ae006ce7690532
Chris <christopher.liebsch@st.ovgu.de>
2023-03-03 14:43:08 +0100

    created 3 constructors

--- src/Map.h
+++ src/Map.h
@@ -4,18 +4,27 @@ class Map
 {
 private:
     /* data */
+    int mapHeight;
+    int mapWidth;
     
 public:
     Map(/* args */);
+    Map(int mapHeight, int mapWidth);
     Map(int mapSize);
     ~Map();
 };
 
 Map::Map(/* args */)
 {    
+
 }
 Map::Map(int mapSize)
 {
+    
+}
+Map::Map(int mapHeight, int mapWidth)
+{
+    
 }
 
 Map::~Map()
